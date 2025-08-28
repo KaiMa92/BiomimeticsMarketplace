272d3d0d1e482debe6c089430d56eb7d9613d0c9
makaiser <makaiser@rhrk.uni-kl.de>
2025-08-28 15:20:59 +0200

    delete unecessary files and configs in app__init__

--- app/__init__.py
+++ app/__init__.py
@@ -9,28 +9,14 @@
 """
 
 from flask import Flask
-from .config.base import Config
-
-#from ragpipeline.load_models import load_gwdg_embedding, load_gwdg_llm
 from ragpipeline.indexmanager.csvindexmanager import OpenalexIndexManager, ScopusIndexManager
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object(Config)  # Load config from Config class
-    app.secret_key = 'your_secret_key'
-    
-    #gwdg_api_key = app.config["GWDG_API_KEY"]
-    #print('GWDG', gwdg_api_key)
-    
-    
-    #app.llm = load_gwdg_llm('qwen3-235b-a22b', api_key= gwdg_api_key)#
-    #app.llm = load_gwdg_llm('mistral-large-instruct', api_key= gwdg_api_key)#
-    #print(app.llm)
-    #app.embedding = load_gwdg_embedding()
+
     app.bio_sim = OpenalexIndexManager(host = 'gwdg', index_path = 'datasets/senckenberg_openalex/index')
-    #app.bio_sim = ScopusIndexManager(llm = app.llm, embedding_model = app.embedding, index_path = 'datasets/senckenberg/index')
     app.bio_sim.top_k = 10#100
-    #app.eng_sim = ScopusIndexManager(llm = app.llm, embedding_model = app.embedding, index_path = 'datasets/livw/index')
+    
     app.eng_sim = OpenalexIndexManager(host = 'gwdg', index_path = 'datasets/livw_openalex/index')
     app.eng_sim.top_k = 10#100
     
